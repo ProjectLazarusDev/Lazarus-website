5c1235df7a6154a9a488a3198e0c514841778124
theonteo <56335249+theonteo@users.noreply.github.com>
2022-01-02 23:36:47 +0800

    trying to fix mobile

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -11,7 +11,7 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import { Grid } from "@mui/material";
 import Card from '@mui/material/Card';
-import { ThemeProvider } from '@mui/material/styles';
+
 import '../Theme/Theme';
 import { themeLight } from '../Theme/Theme';
 import Unity, { UnityContext } from "react-unity-webgl";
@@ -72,48 +72,14 @@ const Home: React.FC = () =>
 
     return (
         <>
-            <Card style={{
-                zIndex: -2, width: '100vw', height: '100%',
-                borderRadius: '0px',
-                background: 'linear-gradient(to right bottom, #121212, #050505)'
-            }}>
-                <Grid
-                    container
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    style={{ height: '100vh' }}
-                >
+           
                     <Unity 
                         unityContext={unityContext}
                         style={{ 
                          borderRadius: '20px', width: "300px", height: "300px" }}
                     />
 
-                </Grid>
-            </Card>
-            <ThemeProvider theme={themeLight}>
-                <Grid
-                    container
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    style={{ height: '100vh' }}
-                >
-                    <Typography align="center" variant="h1" component="h1"
-                        gutterBottom fontFamily='Varela Round' fontWeight='bold' color='black'>
-                        BOBOTS.
-                    </Typography>
-                    <Typography style={{ width: '20vw' }} align="center" variant="body1" component="h1"
-                        gutterBottom fontFamily='Varela Round' fontWeight='light' color='black'>
-                        Engine Generated Collection with a collection of 10,000 bobots.
-                        Keep hyping,keep vibing.
-                    </Typography>
-
-                </Grid>
-            </ThemeProvider>
+               
         </>
     );
 }
