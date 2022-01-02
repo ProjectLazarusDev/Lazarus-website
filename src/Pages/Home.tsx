1196e0edede614b46d1bacae514bb562d040b07f
theonteo <56335249+theonteo@users.noreply.github.com>
2022-01-02 22:18:52 +0800

    loading screen / scrolling fix

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -15,6 +15,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import '../Theme/Theme';
 import { themeLight } from '../Theme/Theme';
 import Unity, { UnityContext } from "react-unity-webgl";
+import './Home.css'
 
 const unityContext = new UnityContext({
     loaderUrl: "devbuild/devbuild.loader.js",
@@ -29,13 +30,51 @@ const Home: React.FC = () =>
     //react hooks
     const [isUnityMounted, setIsUnityMounted] = React.useState<boolean>(true);
     const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
-    const [progression, setProgression] =React. useState<number>(0);
+    const [progression, setProgression] = React.useState<number>(0);
+
+    // Built-in event invoked when the Unity app's progress has changed.
+    function handleOnUnityProgress(progression: number)
+    {
+        setProgression(progression);
+    }
+
+    // Built-in event invoked when the Unity app is loaded.
+    function handleOnUnityLoaded()
+    {
+
+        setIsLoaded(true);
+    }
+    // Event invoked when the user clicks the button, the unity container will be
+    // mounted or unmounted depending on the current mounting state.
+    function handleOnClickUnMountUnity()
+    {
+        if (isLoaded === true)
+        {
+            setIsLoaded(false);
+        }
+        setIsUnityMounted(isUnityMounted === false);
+    }
+
+
+    // When the component is mounted, we'll register some event listener.
+    React.useEffect(() =>
+    {
+        unityContext.on("progress", handleOnUnityProgress);
+        unityContext.on("loaded", handleOnUnityLoaded);
+
+        return function ()
+        {
+            unityContext.removeAllEventListeners();
+        };
+    }, []);
+
+
 
     return (
         <>
             <Card style={{
                 zIndex: -2, width: '100vw', height: '100%',
-                 borderRadius: '0px',
+                borderRadius: '0px',
                 background: 'linear-gradient(to right bottom, #121212, #050505)'
             }}>
                 <Grid
@@ -46,12 +85,23 @@ const Home: React.FC = () =>
                     justifyContent="center"
                     style={{ height: '100vh' }}
                 >
-                    
-                    <Unity  className="unityWindow"
+                    {isLoaded === false && (
+                        <div className="progress-bar">
+                            <div
+                                className="progress-bar-fill"
+                                style={{  width: progression * 100 + "%" }}
+                            />
+                        </div>
+                    )}
+
+                    <Unity className="unityWindow"
                         unityContext={unityContext}
                         matchWebGLToCanvasSize={true}
-                        style={{ borderRadius: '20px', width: "90vw", height: "400px", }}
+                        style={{ visibility: isLoaded ? "visible" : "hidden",
+                         borderRadius: '20px', width: "90vw", height: "400px", }}
                     />
+
+
                 </Grid>
             </Card>
             <ThemeProvider theme={themeLight}>
@@ -64,13 +114,13 @@ const Home: React.FC = () =>
                     style={{ height: '100vh' }}
                 >
                     <Typography align="center" variant="h1" component="h1"
-                        gutterBottom  fontFamily='Varela Round' fontWeight='bold' color='black'>
+                        gutterBottom fontFamily='Varela Round' fontWeight='bold' color='black'>
                         BOBOTS.
                     </Typography>
-                    <Typography style={{ width :'20vw' }} align="center" variant="body1" component="h1"
-                        gutterBottom  fontFamily='Varela Round' fontWeight='light' color='black'>
-                       Engine Generated Collection with a collection of 10,000 bobots. 
-Keep hyping,keep vibing.
+                    <Typography style={{ width: '20vw' }} align="center" variant="body1" component="h1"
+                        gutterBottom fontFamily='Varela Round' fontWeight='light' color='black'>
+                        Engine Generated Collection with a collection of 10,000 bobots.
+                        Keep hyping,keep vibing.
                     </Typography>
 
                 </Grid>
