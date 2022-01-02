bb575c08bc85625ff8d55956d7a95223bc46c5cd
theonteo <56335249+theonteo@users.noreply.github.com>
2022-01-02 21:30:22 +0800

    new main page style

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -12,7 +12,9 @@ import Typography from '@mui/material/Typography';
 import { Grid } from "@mui/material";
 import Card from '@mui/material/Card';
 import { Box } from '@mui/system';
+import { ThemeProvider } from '@mui/material/styles';
 import '../Theme/Theme';
+import { themeLight } from '../Theme/Theme';
 import Unity, { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
@@ -20,67 +22,56 @@ const unityContext = new UnityContext({
     dataUrl: "devbuild/devbuild.data",
     frameworkUrl: "devbuild/devbuild.framework.js",
     codeUrl: "devbuild/devbuild.wasm",
-  });
+});
 
 
 
-const Home: React.FC = () => {
-    
-
-        
-
-        return (
-            <>
-            
-                <Card style={{ zIndex: -2, width: '100vw', height: '100vh', position: "absolute", borderRadius: '0px', background: 'linear-gradient(to right bottom, #87A9FF, #6079BC)' }}></Card>
+const Home: React.FC = () =>
+{
+    return (
+        <>
+            <Card style={{
+                zIndex: -2, width: '100vw', height: '100%',
+                 borderRadius: '0px',
+                background: 'linear-gradient(to right bottom, #121212, #050505)'
+            }}>
                 <Grid
-                                container
-                                spacing={0}
-                                direction="column"
-                                alignItems="center"
-                                justifyContent="center"
-                                style={{ height: '60vh' }}
-                            >
-                                          <Unity
-      unityContext={unityContext}
-      matchWebGLToCanvasSize={true}
-    
-      style={{width: "400px", height: "400px", }}
-    />
-                                    </Grid>
-         
-          
-                            <Grid
-                                container
-                                spacing={0}
-                                direction="column"
-                                alignItems="center"
-                                justifyContent="center"
-                                style={{ height: '60vh' }}
-                            >
-                                                       
-                                < Box
-                                    component="img"
-                                    sx={{
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    style={{ height: '100vh' }}
+                >
+                    <Unity
+                        unityContext={unityContext}
+                        matchWebGLToCanvasSize={true}
+                        style={{ borderRadius: '20px', width: "90vw", height: "400px", }}
+                    />
+                </Grid>
+            </Card>
+            <ThemeProvider theme={themeLight}>
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    style={{ height: '100vh' }}
+                >
+                    <Typography align="center" variant="h1" component="h1"
+                        gutterBottom  fontFamily='Varela Round' fontWeight='bold' color='black'>
+                        BOBOTS.
+                    </Typography>
+                    <Typography style={{ width :'20vw' }} align="center" variant="body1" component="h1"
+                        gutterBottom  fontFamily='Varela Round' fontWeight='light' color='black'>
+                       Engine Generated Collection with a collection of 10,000 bobots. 
+Keep hyping,keep vibing.
+                    </Typography>
 
-                                        maxHeight: { xs: 233, md: 167 },
-                                        maxWidth: { xs: 350, md: 250 },
-                                    }}
-                                    alt="The house from the offer."
-                                    src="logo.png"
-                                >
-                                   
-                                    </Box>
-                                
-
-                                <Typography align="center" variant="body1" component="h1"
-                                    gutterBottom fontFamily='jost' fontWeight='light' color='white'>
-                                    BOBOTS. Website
-                                </Typography>
-
-                            </Grid>
-                        
-            </>
-        );
-    }
+                </Grid>
+            </ThemeProvider>
+        </>
+    );
+}
 export default Home;
