b5b84f34f7163150629bd147657e37be5c35332a
OskyJr <49112599+OskyJr@users.noreply.github.com>
2022-05-01 12:16:22 +0800

    Fixed the popup error from react-webgl-unity when switching pages

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,134 +1,215 @@
-import React from "react"
-
+import React from 'react';
 
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 
-import { FaDiscord, FaTwitter, FaHome,FaPlay } from "react-icons/fa";
-import { Button } from "@mui/material";
+import { FaDiscord, FaTwitter, FaHome, FaPlay } from 'react-icons/fa';
+import { Button } from '@mui/material';
 import { isMobile } from 'react-device-detect';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
-const Header: React.FC = () =>
-{
+import { unityContext, unityContextSeason0 } from '../Context/UnityContext';
+
+const Header: React.FC = () => {
   const [width, setWidth] = React.useState(window.innerWidth);
 
-  React.useEffect(() =>
-  {
-
-    window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
+  React.useEffect(() => {
+    window.addEventListener('resize', updateDimensions);
+    return () => window.removeEventListener('resize', updateDimensions);
   }, []);
-  const updateDimensions = () =>
-  {
+  const updateDimensions = () => {
     setWidth(window.innerWidth);
-
-  }
+  };
   const history = useHistory();
 
-  function handleHome()
-  {
-    history.push("/");
+  // placeholder code to fix the popup error caused by unity 2021.2 ver with react-unity-webgl
+  // https://github.com/jeffreylanters/react-unity-webgl/issues/250
+  const unityUnload = async () => {
+    await unityContext.quitUnityInstance();
+    await unityContextSeason0.quitUnityInstance();
+  };
+
+  async function handleHome() {
+    unityUnload()
+      .then(() => {
+        history.push('/');
+      })
+      .catch(console.error);
   }
 
-  function handleDiscord()
-  {
-    
+  function handleDiscord() {
     window.open('https://discord.gg/jbBzyemqk7');
   }
-  function handleTwitter()
-  {
+  function handleTwitter() {
     window.open('https://twitter.com/BobotsNFT');
   }
-  //TODO: route to /play
-  function handlePlay()
-  {
-    history.push("/play");
+
+  async function handlePlay() {
+    unityUnload()
+      .then(() => {
+        history.push('/play');
+      })
+      .catch(console.error);
   }
 
   const RenderIcons = (
     <>
-      
-      <Button onClick={handleDiscord} variant="contained" style={{ height: '40px', width: '40px', fontFamily: 'Dongle', fontSize: '1.5rem', backgroundColor: '#00000055', borderRadius: '20px', boxShadow: 'none', margin: '5px' }} >
+      <Button
+        onClick={handleDiscord}
+        variant="contained"
+        style={{
+          height: '40px',
+          width: '40px',
+          fontFamily: 'Dongle',
+          fontSize: '1.5rem',
+          backgroundColor: '#00000055',
+          borderRadius: '20px',
+          boxShadow: 'none',
+          margin: '5px',
+        }}
+      >
         <FaDiscord />
       </Button>
-      <Button onClick={handleTwitter} variant="contained" style={{ height: '40px', width: '40px', fontFamily: 'Dongle', fontSize: '1.5rem', backgroundColor: '#00000055', borderRadius: '20px', boxShadow: 'none', margin: '5px' }} >
+      <Button
+        onClick={handleTwitter}
+        variant="contained"
+        style={{
+          height: '40px',
+          width: '40px',
+          fontFamily: 'Dongle',
+          fontSize: '1.5rem',
+          backgroundColor: '#00000055',
+          borderRadius: '20px',
+          boxShadow: 'none',
+          margin: '5px',
+        }}
+      >
         <FaTwitter />
       </Button>
 
-
-      <Button onClick={handleHome} variant="contained" style={{ height: '40px', width: '40px', fontFamily: 'Dongle', fontSize: '1.5rem', backgroundColor: '#00000055', borderRadius: '20px', boxShadow: 'none', margin: '5px' }} >
+      <Button
+        onClick={handleHome}
+        variant="contained"
+        style={{
+          height: '40px',
+          width: '40px',
+          fontFamily: 'Dongle',
+          fontSize: '1.5rem',
+          backgroundColor: '#00000055',
+          borderRadius: '20px',
+          boxShadow: 'none',
+          margin: '5px',
+        }}
+      >
         <FaHome />
       </Button>
 
-      {isMobile === false ? 
-        <Button onClick={handlePlay} variant="contained" style={{height: '100px', width: '100px', fontFamily: 'Dongle', fontSize: '1.5rem', backgroundColor: '#000000aa', borderRadius: '20px', boxShadow: 'none', margin: '5px' }}>
-         <FaPlay />
+      {isMobile === false ? (
+        <Button
+          onClick={handlePlay}
+          variant="contained"
+          style={{
+            height: '100px',
+            width: '100px',
+            fontFamily: 'Dongle',
+            fontSize: '1.5rem',
+            backgroundColor: '#000000aa',
+            borderRadius: '20px',
+            boxShadow: 'none',
+            margin: '5px',
+          }}
+        >
+          <FaPlay />
         </Button>
-      : null}
-
-    </>);
+      ) : null}
+    </>
+  );
   const RenderIconsMobile = (
     <>
-      <Button onClick={handleDiscord} variant="contained" style={{ height: '40px', width: '40px', fontFamily: 'Dongle', fontSize: '1.5rem', backgroundColor: '#00000055', borderRadius: '20px', boxShadow: 'none', margin: '5px' }} >
+      <Button
+        onClick={handleDiscord}
+        variant="contained"
+        style={{
+          height: '40px',
+          width: '40px',
+          fontFamily: 'Dongle',
+          fontSize: '1.5rem',
+          backgroundColor: '#00000055',
+          borderRadius: '20px',
+          boxShadow: 'none',
+          margin: '5px',
+        }}
+      >
         <FaDiscord />
       </Button>
-      <Button onClick={handleTwitter} variant="contained" style={{ height: '40px', width: '40px', fontFamily: 'Dongle', fontSize: '1.5rem', backgroundColor: '#00000055', borderRadius: '20px', boxShadow: 'none', margin: '5px' }} >
+      <Button
+        onClick={handleTwitter}
+        variant="contained"
+        style={{
+          height: '40px',
+          width: '40px',
+          fontFamily: 'Dongle',
+          fontSize: '1.5rem',
+          backgroundColor: '#00000055',
+          borderRadius: '20px',
+          boxShadow: 'none',
+          margin: '5px',
+        }}
+      >
         <FaTwitter />
       </Button>
 
-
-      <Button onClick={handleHome} variant="contained" style={{ height: '40px', width: '40px', fontFamily: 'Dongle', fontSize: '1.5rem', backgroundColor: '#00000055', borderRadius: '20px', boxShadow: 'none', margin: '5px' }} >
+      <Button
+        onClick={handleHome}
+        variant="contained"
+        style={{
+          height: '40px',
+          width: '40px',
+          fontFamily: 'Dongle',
+          fontSize: '1.5rem',
+          backgroundColor: '#00000055',
+          borderRadius: '20px',
+          boxShadow: 'none',
+          margin: '5px',
+        }}
+      >
         <FaHome />
       </Button>
-
-    </>);
+    </>
+  );
   const Desk = (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed" style={{paddingTop:'20px', boxShadow: 'none', backgroundColor: 'transparent' }} >
+        <AppBar position="fixed" style={{ paddingTop: '20px', boxShadow: 'none', backgroundColor: 'transparent' }}>
           <Toolbar>
             <Box sx={{ flexGrow: 1 }} />
-            <Box >
-              {RenderIcons}
-            </Box>
+            <Box>{RenderIcons}</Box>
           </Toolbar>
         </AppBar>
-
       </Box>
-
-    </>);
+    </>
+  );
   const Mobile = (
     <>
-
-      <AppBar position="fixed" style={{ boxShadow: 'none', backgroundColor: 'transparent' }} >
+      <AppBar position="fixed" style={{ boxShadow: 'none', backgroundColor: 'transparent' }}>
         <Toolbar>
-          <Box sx={{ flexGrow: 0.5 }}>
-          </Box>
+          <Box sx={{ flexGrow: 0.5 }}></Box>
           {RenderIconsMobile}
         </Toolbar>
       </AppBar>
-    </>);
+    </>
+  );
   return (
     <>
       {
         <div>
-
           {width >= 960 && Desk}
           {width < 960 && Mobile}
-
-
         </div>
       }
-
     </>
   );
-
-}
-
-
-
-
+};
 
 export default Header;
--- src/Pages/Multiplayer.tsx
+++ src/Pages/Multiplayer.tsx
@@ -7,29 +7,28 @@
 */
 /*****************************************************************************/
 
-import React from "react";
-import { Grid } from "@mui/material";
-import Card from "@mui/material/Card";
-import "../Theme/Theme";
+import React from 'react';
+import { Grid } from '@mui/material';
+import Card from '@mui/material/Card';
+import '../Theme/Theme';
 
-import "./Home.css";
-import "./Page.css";
-import Typography from "@mui/material/Typography";
-import Header from "../Components/Header";
+import './Home.css';
+import './Page.css';
+import Typography from '@mui/material/Typography';
+import Header from '../Components/Header';
 
-import "motion-pointer/dist/index.css";
-import "motion-pointer/dist/index.js";
-import { isMobile } from "react-device-detect";
-import "../indexweb3.js";
-import GameScreen from "../Components/GameScreen";
+import 'motion-pointer/dist/index.css';
+import 'motion-pointer/dist/index.js';
+import { isMobile } from 'react-device-detect';
+import '../indexweb3.js';
+import GameScreen from '../Components/GameScreen';
 
-import * as blockchain from "../Blockchain/BlockchainFunctions";
+import * as blockchain from '../Blockchain/BlockchainFunctions';
 
-import  {unityContextSeason0}  from "../Context/UnityContext";
+import { unityContextSeason0 } from '../Context/UnityContext';
 //abi import
 
-const Multiplayer: React.FC = () =>
-{
+const Multiplayer: React.FC = () => {
   //react hooks
   const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
   const [progression, setProgression] = React.useState<number>(0);
@@ -37,61 +36,47 @@ const Multiplayer: React.FC = () =>
 
   //store eth addresses
 
-  React.useEffect(() =>
-  {
-    const scrollFun = () =>
-    {
-      setScrollValue(
-        -document.body.getBoundingClientRect().top /
-        document.body.getBoundingClientRect().height
-      );
+  React.useEffect(() => {
+    const scrollFun = () => {
+      setScrollValue(-document.body.getBoundingClientRect().top / document.body.getBoundingClientRect().height);
       //unityContextSeason0.send("MainMenuControl", "SetScrollBarValue", scrollValue);
     };
-    window.addEventListener("scroll", scrollFun);
+    window.addEventListener('scroll', scrollFun);
 
-    return () =>
-    {
-      window.removeEventListener("scroll", scrollFun);
+    return () => {
+      window.removeEventListener('scroll', scrollFun);
     };
   }, [scrollValue]);
 
   // Built-in event invoked when the Unity app's progress has changed.
-  function handleOnUnityProgress(progression: number)
-  {
+  function handleOnUnityProgress(progression: number) {
     setProgression(progression);
   }
 
   // Built-in event invoked when the Unity app is loaded.
-  function handleOnUnityLoaded()
-  {
-    document.body.style.overflowY = "scroll";
+  function handleOnUnityLoaded() {
+    document.body.style.overflowY = 'scroll';
     document.documentElement.scrollTop = 0;
     setIsLoaded(true);
-    setScrollValue(
-      -document.body.getBoundingClientRect().top /
-      document.body.getBoundingClientRect().height
-    );
+    setScrollValue(-document.body.getBoundingClientRect().top / document.body.getBoundingClientRect().height);
     unityContextSeason0.setFullscreen(true);
   }
 
-  const updateDimensions = () => { };
+  const updateDimensions = () => {};
 
-  React.useEffect(() =>
-  {
-    unityContextSeason0.on("progress", handleOnUnityProgress);
-    unityContextSeason0.on("loaded", handleOnUnityLoaded);
-    unityContextSeason0.on("quitted", function () { });
-    document.body.style.overflowY = "hidden";
-    window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
+  React.useEffect(() => {
+    unityContextSeason0.on('progress', handleOnUnityProgress);
+    unityContextSeason0.on('loaded', handleOnUnityLoaded);
+    unityContextSeason0.on('quitted', function () {});
+    document.body.style.overflowY = 'hidden';
+    window.addEventListener('resize', updateDimensions);
+    return () => window.removeEventListener('resize', updateDimensions);
   }, []);
 
   // When the component is mounted, we'll register some event listener.
-  React.useEffect(() =>
-  {
+  React.useEffect(() => {
     blockchain.BindToContext();
-    return function ()
-    {
+    return function () {
       // handleOnClickUnMountUnity();
       unityContextSeason0.removeAllEventListeners();
     };
@@ -106,29 +91,28 @@ const Multiplayer: React.FC = () =>
         <Card
           style={{
             zIndex: isLoaded ? -2 : 20,
-            position: "fixed",
-            width: "100vw",
-            height: "100vh",
-            background: "linear-gradient(to right bottom, #444444ff,#000000ff)",
-            borderRadius: "0px",
-            alignItems: " center",
-            justifyContent: " center",
+            position: 'fixed',
+            width: '100vw',
+            height: '100vh',
+            background: 'linear-gradient(to right bottom, #444444ff,#000000ff)',
+            borderRadius: '0px',
+            alignItems: ' center',
+            justifyContent: ' center',
           }}
         ></Card>
         <Card
           style={{
-            boxShadow: "none",
+            boxShadow: 'none',
             zIndex: -2,
-            width: "100vw",
-            height: "90vh",
-            borderRadius: "0px",
-            background:
-              "linear-gradient(to right bottom, #12121200, #05050500)",
+            width: '100vw',
+            height: '90vh',
+            borderRadius: '0px',
+            background: 'linear-gradient(to right bottom, #12121200, #05050500)',
           }}
         >
           {isMobile === false ? (
             <GameScreen isLoaded={isLoaded} progression={progression}>
-              {" "}
+              {' '}
             </GameScreen>
           ) : (
             <Grid
@@ -138,17 +122,17 @@ const Multiplayer: React.FC = () =>
               alignItems="center"
               justifyContent="center"
               style={{
-                borderRadius: "0px",
-                height: "100vh",
-                boxShadow: "none",
+                borderRadius: '0px',
+                height: '100vh',
+                boxShadow: 'none',
               }}
             >
               <div className="ui-text" style={{ zIndex: isLoaded ? -2 : 21 }}>
                 <Typography
-                  paddingBottom={"50px"}
-                  paddingTop={"25px"}
+                  paddingBottom={'50px'}
+                  paddingTop={'25px'}
                   fontFamily="Dongle"
-                  letterSpacing={"5px"}
+                  letterSpacing={'5px'}
                   lineHeight={0}
                   color="#ffffffff"
                   fontWeight="bold"
