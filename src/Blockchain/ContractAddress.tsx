b137103b7195098d3cd64f3a9da074e82d95f453
OskyJr <49112599+OskyJr@users.noreply.github.com>
2022-06-06 00:28:54 +0800

    updated installationCoreChamber contract address

--- src/Blockchain/ContractAddress.tsx
+++ src/Blockchain/ContractAddress.tsx
@@ -1,4 +1,4 @@
 //put contract addr here - localhost or remix
 export const bobotGenesisAddress: string = '0x1eFD560A620c3F341d0646f5Ec01A46ffa353f86';
-export const installationCoreChamberAddress: string = '0xbDd14A897d37d11f6FEA422319d8D7ea606F461A';
+export const installationCoreChamberAddress: string = '0x3e8bb6F0534C5C98dcCDCA774558f56C940E12d7';
 export const magic20Address: string = '0x539bdE0d7Dbd336b79148AA742883198BBF60342';
