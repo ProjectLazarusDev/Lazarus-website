06da83583c6795d6948716f5360d63a39270ea02
neoqiwen <neoqiwen@gmail.com>
2022-06-10 00:38:45 +0800

    Update ContractAddress.tsx

--- src/Blockchain/ContractAddress.tsx
+++ src/Blockchain/ContractAddress.tsx
@@ -1,4 +1,4 @@
 //put contract addr here - localhost or remix
-export const bobotGenesisAddress: string = '0xB699cC74f969A5A9DA4FcC41107d45063EaBcF34';
+export const bobotGenesisAddress: string = '0x3e24D2eedEf4090dcc0efC7537b50Ba96946ea81';
 export const installationCoreChamberAddress: string = '0xEC946586c88864ceDB9a502CA1C14d71ADE8191f';
 export const magic20Address: string = '0x539bdE0d7Dbd336b79148AA742883198BBF60342';
