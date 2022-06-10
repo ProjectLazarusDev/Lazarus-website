bc54d502b2f4d0ae6aace414e076773a2db1745c
neoqiwen <neoqiwen@gmail.com>
2022-06-11 02:02:01 +0800

    Revert "Update ContractAddress.tsx"
    This reverts commit a62cab29bca6b4d418b14a07d4e0b7b293b93e7f.

--- src/Blockchain/ContractAddress.tsx
+++ src/Blockchain/ContractAddress.tsx
@@ -1,4 +1,4 @@
 //put contract addr here - localhost or remix
-export const bobotGenesisAddress: string = '0x4c365678fA084bA2447dDEBdB009dF2C46A0A5f8';
+export const bobotGenesisAddress: string = '0x3e24D2eedEf4090dcc0efC7537b50Ba96946ea81';
 export const installationCoreChamberAddress: string = '0xEC946586c88864ceDB9a502CA1C14d71ADE8191f';
 export const magic20Address: string = '0x539bdE0d7Dbd336b79148AA742883198BBF60342';
