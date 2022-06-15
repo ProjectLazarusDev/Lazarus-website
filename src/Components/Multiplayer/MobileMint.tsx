73753cb7d3530bd8f18370a474075a31d585f394
neoqiwen <neoqiwen@gmail.com>
2022-06-16 00:58:32 +0800

    fixed minting issue

--- src/Components/Multiplayer/MobileMint.tsx
+++ src/Components/Multiplayer/MobileMint.tsx
@@ -7,13 +7,9 @@ import Typography from '@mui/material/Typography';
 import 'motion-pointer/dist/index.css';
 import 'motion-pointer/dist/index.js';
 import { Button } from '@mui/material';
-
 import { CardMedia } from '@mui/material';
-
-import * as metaLogin from '../../Blockchain/MetaMaskLogin';
+import { MetaLogin } from '../../Blockchain/MetaMaskLogin';
 import * as bootUpStation from '../../Blockchain/BootUpStation';
-import * as bobots from '../../Blockchain/Bobots';
-import * as user from '../../Blockchain/User';
 
 interface MobileMint {
   message: string;
@@ -21,12 +17,14 @@ interface MobileMint {
 }
 
 const MobileMint: React.FC<MobileMint> = (props) => {
+  // log in manually first as we need to access the 
+  // metamask account wallet for minting verification later on
+  MetaLogin();
+
   return (
-      
     <Grid
       container
       spacing={0}
-
       alignItems="center"
       justifyContent="center"
       style={{
@@ -36,37 +34,35 @@ const MobileMint: React.FC<MobileMint> = (props) => {
         height: '100vh',
         boxShadow: 'none',
       }}
-    >  
-      <div className="mint" style={{ zIndex:  21 }}>
-      <Grid
-      container
-      spacing={0}
-
-      alignItems="center"
-      justifyContent="center"
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        borderRadius: '0px',
-        boxShadow: 'none',
-      }}
-    >  
-      <CardMedia
-        component="video"
-        muted={true}
-        image={'mint/video_unrevealed.mp4'}
-        autoPlay
-        loop
-        style={{
-      
-          zIndex:10,
-          margin: '5px',
-          width: '250px',
-          height: '250px',
-          borderRadius: '50px',
-        }}
-      />
-       </Grid>
+    >
+      <div className="mint" style={{ zIndex: 21 }}>
+        <Grid
+          container
+          spacing={0}
+          alignItems="center"
+          justifyContent="center"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            borderRadius: '0px',
+            boxShadow: 'none',
+          }}
+        >
+          <CardMedia
+            component="video"
+            muted={true}
+            image={'mint/video_unrevealed.mp4'}
+            autoPlay
+            loop
+            style={{
+              zIndex: 10,
+              margin: '5px',
+              width: '250px',
+              height: '250px',
+              borderRadius: '50px',
+            }}
+          />
+        </Grid>
         <Typography
           paddingBottom={'50px'}
           paddingTop={'25px'}
@@ -78,24 +74,24 @@ const MobileMint: React.FC<MobileMint> = (props) => {
           variant="subtitle1"
           fontSize="1.5rem"
         >
-         BOBOTS GENESIS
+          BOBOTS GENESIS
         </Typography>
         <Button
-                                onClick={bootUpStation.MintBobot}
-                              variant="contained"
-                              style={{
-                                height: '40px',
-                                width: '80px',
-                                fontFamily: 'Dongle',
-                                fontSize: '1.5rem',
-                                backgroundColor: '#ebbd34cc',
-                                borderRadius: '20px',
-                                boxShadow: 'none',
-                                margin: '5px',
-                              }}
-                            >
-                            MINT
-                            </Button>
+          onClick={bootUpStation.MintBobot}
+          variant="contained"
+          style={{
+            height: '40px',
+            width: '80px',
+            fontFamily: 'Dongle',
+            fontSize: '1.5rem',
+            backgroundColor: '#ebbd34cc',
+            borderRadius: '20px',
+            boxShadow: 'none',
+            margin: '5px',
+          }}
+        >
+          MINT
+        </Button>
       </div>
     </Grid>
   );
