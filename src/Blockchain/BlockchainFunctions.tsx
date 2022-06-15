11f8a4df05fe27d067bbf3574812d28e720be191
OskyJr <49112599+OskyJr@users.noreply.github.com>
2022-06-16 01:50:28 +0800

    Mint loading (#9)
    * added retrieving of stake status for all owned bobots during bootup loading
    
    * Update BlockchainFunctions.tsx
    
    * updated with webgl build to retrieve from new backend uri for metadata and pinata uri for images
    
    * abstracted blockchain callbacks to BlockchainSender.tsx and added loading circle ui when minting
    
    * fix warning

--- src/Blockchain/BlockchainFunctions.tsx
+++ src/Blockchain/BlockchainFunctions.tsx
@@ -5,8 +5,6 @@ import * as bootUpStation from './BootUpStation';
 import * as bobots from './Bobots';
 import * as user from './User';
 
-const blockchainManager: string = 'BlockchainManager';
-
 export enum BlockchainError {
   NoError = 0,
   NetworkBusy,
@@ -20,11 +18,13 @@ export function BindToContext() {
   //mint
   unityContext.on('Mint_Request', bootUpStation.MintBobot);
 
+  //uri request
+  unityContext.on('GetAllTokenURIs_Request', bobots.GetBobotsAllURI);
+
   //stake
   unityContext.on('Stake_Request', bootUpStation.StakeBobot);
 
-  //uri request
-  unityContext.on('GetAllTokenURIs_Request', bobots.GetBobotsAllURI);
+  unityContext.on('GetAllTokenStakeStatus_Request', bobots.GetBobotsAllStakeStatus);
 
   //address and magic
   unityContext.on('GetUserData', user.GetUserData);
@@ -33,57 +33,4 @@ export function BindToContext() {
   unityContext.on('OpenURL_Request', function (url: string) {
     window.open(url);
   });
-}
-
-//TODO: abstract to a BlockchainSender?
-// Communication from React to Unity with unityContext.send(...)
-export function Log_Callback(_message: string) {
-  unityContext.send(blockchainManager, 'Log_Callback', _message);
-}
-
-export function LoadingScreenToggle_Callback(_b: any) {
-  unityContext.send(blockchainManager, 'LoadingScreenToggle_Callback', _b);
-}
-
-export function MetaMaskLogin_Callback(_address: string) {
-  unityContext.send(blockchainManager, 'MetaMaskLogin_Callback', _address);
-}
-
-export function GetMagic_Callback(_magic: number) {
-  unityContext.send(blockchainManager, 'GetMagic_Callback', _magic);
-}
-
-export function Mint_Callback(_errorCode: number) {
-  unityContext.send(blockchainManager, 'Mint_Callback', _errorCode);
-}
-
-export function GetAllTokenURIs_Callback(_errorCode: number) {
-  unityContext.send(blockchainManager, 'GetAllTokenURIs_Callback', _errorCode);
-}
-
-export function RecieveTokenURI_Callback(_tokenURI: string) {
-  unityContext.send(blockchainManager, 'RecieveTokenURI_Callback', _tokenURI);
-}
-
-export function CompletedTokenURI_Callback() {
-  unityContext.send(blockchainManager, 'CompletedTokenURI_Callback');
-}
-
-export function GetAllTokenStakeStatus_Callback(_errorCode: number) {
-  unityContext.send(blockchainManager, 'GetAllTokenStakeStatus_Callback', _errorCode);
-}
-
-export function ReceiveTokenStakeStatus_Callback(tokenId: number, stakeStatus: boolean, currentCorePoints: number) {
-  const stakeStatusData = {
-    _tokenId: tokenId,
-    _stakeStatus: stakeStatus,
-    _currentCorePoints: currentCorePoints,
-  };
-  const jsonData = JSON.stringify(stakeStatusData);
-  //TODO: fix spelling error here and on unity side
-  unityContext.send(blockchainManager, 'RecieveTokenStakeStatus_Callback', jsonData);
-}
-
-export function CompletedTokenStakeStatus_Callback() {
-  unityContext.send(blockchainManager, 'CompletedTokenStakeStatus_Callback');
 }
---
+++ src/Blockchain/BlockchainSender.tsx
@@ -0,0 +1,60 @@
+
+import { unityContext } from '../Context/UnityContext';
+
+
+const blockchainManager: string = 'BlockchainManager';
+
+// Communication from React to Unity with unityContext.send(...)
+// the single parameter can be a string, a number, or can be empty.
+// https://docs.unity3d.com/Manual/webgl-interactingwithbrowserscripting.html
+export function Log_Callback(_message: string) {
+  unityContext.send(blockchainManager, 'Log_Callback', _message);
+}
+
+export function LoadingScreenToggle_Callback(_b: boolean) {
+  let flag = _b ? 1 : 0;
+  unityContext.send(blockchainManager, 'LoadingScreenToggle_Callback', flag);
+}
+
+export function MetaMaskLogin_Callback(_address: string) {
+  unityContext.send(blockchainManager, 'MetaMaskLogin_Callback', _address);
+}
+
+export function GetMagic_Callback(_magic: number) {
+  unityContext.send(blockchainManager, 'GetMagic_Callback', _magic);
+}
+
+export function Mint_Callback(_errorCode: number) {
+  unityContext.send(blockchainManager, 'Mint_Callback', _errorCode);
+}
+
+export function GetAllTokenURIs_Callback(_errorCode: number) {
+  unityContext.send(blockchainManager, 'GetAllTokenURIs_Callback', _errorCode);
+}
+
+export function RecieveTokenURI_Callback(_tokenURI: string) {
+  unityContext.send(blockchainManager, 'RecieveTokenURI_Callback', _tokenURI);
+}
+
+export function CompletedTokenURI_Callback() {
+  unityContext.send(blockchainManager, 'CompletedTokenURI_Callback');
+}
+
+export function GetAllTokenStakeStatus_Callback(_errorCode: number) {
+  unityContext.send(blockchainManager, 'GetAllTokenStakeStatus_Callback', _errorCode);
+}
+
+export function ReceiveTokenStakeStatus_Callback(tokenId: number, stakeStatus: boolean, currentCorePoints: number) {
+  const stakeStatusData = {
+    _tokenId: tokenId,
+    _stakeStatus: stakeStatus,
+    _currentCorePoints: currentCorePoints,
+  };
+  const jsonData = JSON.stringify(stakeStatusData);
+  //TODO: fix spelling error here and on unity side
+  unityContext.send(blockchainManager, 'RecieveTokenStakeStatus_Callback', jsonData);
+}
+
+export function CompletedTokenStakeStatus_Callback() {
+  unityContext.send(blockchainManager, 'CompletedTokenStakeStatus_Callback');
+}
