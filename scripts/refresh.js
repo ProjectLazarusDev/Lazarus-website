9e2a8824d647edf7a78a5db945b3c8b4511bd003
neoqiwen <neoqiwen@gmail.com>
2022-07-07 00:19:11 +0800

    Update refresh.js

--- scripts/refresh.js
+++ scripts/refresh.js
@@ -1,14 +1,15 @@
 //import axios from 'axios';
 const axios = require('axios').default;
 
-const mintedCount = 2865;
+const mintedCount = 4040;
+const troveUrl = 'https://hfihu314z3.execute-api.us-east-1.amazonaws.com/collection/arb/bobots-genesis/';
 
 async function main() {
-    console.log("starting refresh...");
+    console.log(`starting refresh at ${troveUrl}`);
 
     for (let tokenId = 1; tokenId <= mintedCount; ++tokenId) {
         try {
-            const response = await axios.get(`https://hfihu314z3.execute-api.us-east-1.amazonaws.com/collection/arb/bobots-genesis/${tokenId}/refresh`);
+            const response = await axios.get(`${troveUrl}${tokenId}/refresh`);
             console.log(`tokenId ${tokenId} refreshed with response ${response?.status} ${response?.statusText}`);
         }
         catch (error) {
