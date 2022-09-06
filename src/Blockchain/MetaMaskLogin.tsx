ebe8d2201ea042f6eb1cc3d6f7f94f0c1dc39910
neoqiwen <neoqiwen@gmail.com>
2022-09-07 07:38:25 +0800

    wip for jwt authentication

--- src/Blockchain/MetaMaskLogin.tsx
+++ src/Blockchain/MetaMaskLogin.tsx
@@ -1,4 +1,5 @@
 import * as blockchainSender from './BlockchainSender';
+import axios from 'axios';
 //sotres metamask accounts
 var MetaMaskAccounts: any;
 
@@ -10,6 +11,17 @@ async function MetaLogin() {
 
     //send connected address back to engine
     blockchainSender.MetaMaskLogin_Callback(MetaMaskAccounts[0]);
+
+    //jwt authentication to get access token with backend server
+    //this accesToken will be passed to frontend to be stored in session storage
+    //an optimization will be to store in httpOnly cookies which is a better approach
+    axios
+      .post('localhost:3001/users/login', MetaMaskAccounts[0])
+      .then((response) => {
+        console.log('hello');
+        sessionStorage.setItem('accessToken', response.data.token);
+      })
+      .catch((error) => console.log(error));
   }
 }
 
