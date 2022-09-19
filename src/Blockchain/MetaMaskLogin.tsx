1665c788a9e1b40f99dd8c97971d8377f1960445
neoqiwen <neoqiwen@gmail.com>
2022-09-19 23:50:27 +0800

    testing out login authentication

--- src/Blockchain/MetaMaskLogin.tsx
+++ src/Blockchain/MetaMaskLogin.tsx
@@ -1,25 +1,32 @@
 import * as blockchainSender from './BlockchainSender';
 import axios from 'axios';
-//sotres metamask accounts
+
+// sotres metamask accounts
 var MetaMaskAccounts: any;
 
-//connect to metamask
+// connect to metamask
 async function MetaLogin() {
   if ((window as any).ethereum) {
     MetaMaskAccounts = await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
     console.log(MetaMaskAccounts[0]);
 
-    //send connected address back to engine
+    // send connected address back to engine
     blockchainSender.MetaMaskLogin_Callback(MetaMaskAccounts[0]);
 
-    //jwt authentication to get access token with backend server
-    //this accesToken will be passed to frontend to be stored in session storage
-    //an optimization will be to store in httpOnly cookies which is a better approach
+    // nonce retrieval
     axios
-      .post('localhost:3001/users/login', MetaMaskAccounts[0])
+      .get(`${process.env.NEXT_PUBLIC_NONCE_ENDPOINT}`, { params: { metamaskAddress: MetaMaskAccounts[0] } })
       .then((response) => {
-        console.log('hello');
-        sessionStorage.setItem('accessToken', response.data.token);
+        let nonce = response;
+        console.log('nonce', nonce, response);
+      })
+      .catch((error) => console.log(error));
+
+    // jwt authentication to get access token with backend server
+    axios
+      .post(`${process.env.NEXT_PUBLIC_LOGIN_ENDPOINT}`, { metamaskAddress: MetaMaskAccounts[0] })
+      .then((response) => {
+        console.log('response', response);
       })
       .catch((error) => console.log(error));
   }
