0054f0158e387476887bc5a763d2b5e606c20b6b
neoqiwen <neoqiwen@gmail.com>
2022-10-02 12:27:50 +0800

    Fixed issue for post request to /login

--- src/Blockchain/MetaMaskLogin.tsx
+++ src/Blockchain/MetaMaskLogin.tsx
@@ -4,23 +4,6 @@ import axios from 'axios';
 // sotres metamask accounts
 var MetaMaskAccounts: any;
 
-// Given a cookie key `name`, returns the value of
-// the cookie or `null`, if the key is not found.
-function getCookie(name: string): string | null {
-  const nameLenPlus = name.length + 1;
-  return (
-    document.cookie
-      .split(';')
-      .map((c) => c.trim())
-      .filter((cookie) => {
-        return cookie.substring(0, nameLenPlus) === `${name}=`;
-      })
-      .map((cookie) => {
-        return decodeURIComponent(cookie.substring(nameLenPlus));
-      })[0] || null
-  );
-}
-
 // connect to metamask
 async function MetaLogin() {
   if ((window as any).ethereum) {
@@ -33,22 +16,29 @@ async function MetaLogin() {
     console.log('process.env.NEXT_PUBLIC_NONCE_ENDPOINT', process.env.NEXT_PUBLIC_NONCE_ENDPOINT, MetaMaskAccounts[0]);
     // nonce retrieval
     axios
-      .get(`${process.env.NEXT_PUBLIC_NONCE_ENDPOINT}`, { params: { metamaskAddress: MetaMaskAccounts[0] } })
+      .get(`${process.env.NEXT_PUBLIC_NONCE_ENDPOINT}`, {
+        params: { metamaskAddress: MetaMaskAccounts[0] },
+        withCredentials: true,
+      })
       .then((response) => {
         console.log('response', response);
-        const nonce = getCookie('nonce');
-        console.log('nonce', nonce, document.cookie);
 
         // jwt authentication to get access token with backend server
         axios
-          .post(`${process.env.NEXT_PUBLIC_LOGIN_ENDPOINT}`, { metamaskAddress: MetaMaskAccounts[0] })
+          .post(
+            `${process.env.NEXT_PUBLIC_LOGIN_ENDPOINT}`,
+            {
+              metamaskAddress: MetaMaskAccounts[0],
+            },
+            {
+              //AxiosRequestConfig parameter
+              withCredentials: true,
+            }
+          )
           .then((response) => {
             console.log('log in successful!', response);
-            const access_token = getCookie('access_token');
-            console.log('access_token', access_token);
             //blockchainSender.GetCookieHeader_Callback();
           })
-          //TODO: check why failing here
           .catch((error) => console.log(error));
       })
       .catch((error) => console.log(error));
