a50f94fcc1bd0bb0fab8f7efa3725b7530291dfa
neoqiwen <neoqiwen@gmail.com>
2022-10-16 21:38:05 +0800

    Fix up

--- src/Data/TeamData.tsx
+++ src/Data/TeamData.tsx
@@ -8,32 +8,28 @@ interface TeamProps {
 const EDMUND: TeamProps = {
     name: 'EDMUND',
     role: '3D ARTIST',
-    description1: 'Experienced 3D Artist with varying expertise from game \
-    art, video editing and commercials to VFX!',
+    description1: 'Experienced 3D Artist with varying expertise from game art, video editing and commercials to VFX!',
     description2: 'Anchoring project from art concept and direction to modelling of 3D Bobots.'
 };
 
 const THEON: TeamProps = {
     name: 'THEON',
     role: ' WEB DEV / FOUNDER',
-    description1: 'Artist turned software engineer. \
-    Ranging experience from 3D Art to custom 3D Engines and web development.',
+    description1: 'Artist turned software engineer. Ranging experience from 3D Art to custom 3D Engines and web development.',
     description2: 'Directing overall project direction and building the website you see now.'
 };
 
 const FARZAANA: TeamProps = {
     name: 'FARZAANA',
     role: 'WEB3 DEVELOPER',
-    description1: 'Artist turned software engineer, from 3D game art to creating \
-    custom game engines.',
+    description1: 'Artist turned software engineer, from 3D game art to creating custom game engines.',
     description2: 'Currently helming as a Blockchain and Smart Contracts Developer.'
 };
 
 const ELSTON: TeamProps = {
     name: 'ELSTON',
     role: 'MARKETING',
-    description1: 'Online marketing professional with many years of sales \
-    background moving into the NFT space.',
+    description1: 'Online marketing professional with many years of sales background moving into the NFT space.',
     description2: 'Strong belief in taking the NFT community to the next level.'
 };
 
@@ -41,8 +37,7 @@ const YIEN: TeamProps = {
     name: 'YIEN',
     role: 'GAME PROGRAMMER',
     description1: 'Experienced Unity Game Programmer with years under his belt.',
-    description2: 'Coding up the multiplayer component and gameplay aspects of \
-    the project. He regards himself as a NPC in real life.'
+    description2: 'Coding up the multiplayer component and gameplay aspects of the project. He regards himself as a NPC in real life.'
 };
 
 const KEFF: TeamProps = {
@@ -55,17 +50,14 @@ const KEFF: TeamProps = {
 const SHERMAN: TeamProps = {
     name: 'SHERMAN',
     role: 'GAME PROGRAMMER',
-    description1: 'Unity developer and game designer that has worked on both \
-    commercial game engines and custom engines.',
+    description1: 'Unity developer and game designer that has worked on both commercial game engines and custom engines.',
     description2: 'Innovating and honing the Bobots experience.'
 };
 
 const QIWEN: TeamProps = {
     name: 'QI WEN',
     role: 'WEB DEVELOPER',
-    description1: 'Assisting in web development and \
-    linking up smart contracts with Unity code for \
-    Metamask transactions.',
+    description1: 'Assisting in web development and linking up smart contracts with Unity code for Metamask transactions.',
     description2: 'Likes bouldering and swimming in his free time.'
 };
 
